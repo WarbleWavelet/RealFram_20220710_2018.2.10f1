f7e6c00ca8c165ce66a5761b1ee64b81a4a0656e
WWS <3027565221@qq.com>
2022-07-11 12:19:44 +0800

    AssetBundle、AssetDataBase加载资源

---
+++ RealFram_20220710_2018.2.10f1/Assets/Editor/AssetBundleEditor.cs
@@ -0,0 +1,59 @@
+﻿using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+public class AssetBundleEditor
+{
+    AssetBundleWriter abWriter;
+
+    void Start()
+    {
+        abWriter = new AssetBundleWriter()
+        {
+            outputPath = "AssetBundles",
+            buildAssetBundleOptions = BuildAssetBundleOptions.None,
+            buildTarget = BuildTarget.StandaloneWindows64
+        };
+        abWriter = new AssetBundleWriter()
+        {
+            outputPath = Application.streamingAssetsPath,//StreamingAssets
+            buildAssetBundleOptions = BuildAssetBundleOptions.ChunkBasedCompression,
+            buildTarget = EditorUserBuildSettings.activeBuildTarget
+        };
+
+
+    }
+
+    [MenuItem("AssetBundles/Build AssetBundles")]//按钮在菜单栏的位置
+    static void BuildAllAssetBundles()
+    {
+        AssetBundleWriter abWriter = new AssetBundleWriter()
+        {
+            outputPath = Application.streamingAssetsPath,//StreamingAssets
+            buildAssetBundleOptions = BuildAssetBundleOptions.ChunkBasedCompression,
+            buildTarget = EditorUserBuildSettings.activeBuildTarget
+        };
+        //
+        if (Directory.Exists(abWriter.outputPath) == false)
+        {
+            Directory.CreateDirectory(abWriter.outputPath);
+        }
+        //目录，模式，平台
+        BuildPipeline.BuildAssetBundles(
+            abWriter.outputPath,
+            abWriter.buildAssetBundleOptions,
+            abWriter.buildTarget
+        );
+        AssetDatabase.Refresh();
+    }
+}
+class AssetBundleWriter
+{
+    /// <summary>目录</summary>
+    public string outputPath;
+    /// <summary>模式</summary>
+    public BuildAssetBundleOptions buildAssetBundleOptions;
+    /// <summary>buildTarget</summary>
+    public BuildTarget buildTarget;
+
+}
---
+++ RealFram_20220710_2018.2.10f1/Assets/Scripts/AssetBundle_Read.cs
@@ -0,0 +1,52 @@
+﻿
+using UnityEngine;
+
+public class AssetBundle_Read : MonoBehaviour 
+{
+
+    void Start()
+    {
+        GameObject go= ReadAssetBundle(DefinePath.path_Stream);
+        go.transform.position = Vector3.left * Constants.offset;
+    }
+
+    GameObject ReadAssetBundle(string path)
+    {
+        AssetBundle ab = AssetBundle.LoadFromFile(path);//必须加后缀
+        string _name =Common.TrimName(path );
+        GameObject prefab = ab.LoadAsset<GameObject>(_name);//可以不加后缀
+        GameObject go = Instantiate(  prefab);
+
+        return go;
+    }
+   
+}
+
+public class DefinePath
+{
+    public static string path_Stream = Application.streamingAssetsPath + "/attack.unity3d";
+    public static string path_ADB =  "Assets/GameData/Prefabs/Attack.prefab";
+}
+
+public class Common
+{
+
+
+    public static string TrimName(string path)
+    {
+        string _name = path.Substring(path.LastIndexOf('/') + 1);// plane.unity3d
+        _name = _name.Substring(0, _name.LastIndexOf('.'));// plane
+
+        return _name;
+    }
+}
+
+
+public class Constants
+{
+    public const float offset = 10f;
+}
+
+
+
+
